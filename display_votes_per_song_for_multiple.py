ceafde024232777ec65d4fa9f2dae6c63422f9e3
Kestevens <118047719+Kestevens@users.noreply.github.com>
2025-05-29 10:16:51 +0200

    Update display_votes_per_song_for_multiple.py

--- display_votes_per_song_for_multiple.py
+++ display_votes_per_song_for_multiple.py
@@ -1,7 +1,33 @@
-import json
+import os, io, json
+import pandas as pd
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaIoBaseDownload, MediaFileUpload
 
-# Define the path to the output file
-input_file = "reduced_votes.json"
+SERVICE_ACCOUNT_FILE = "/root/.config/service_account.json"
+INPUT_FOLDER_ID = "1_vr56jMd4aQaahI_bUvSRYcdxyGHY8zG"
+OUTPUT_FOLDER_ID = "16AdOIvSlwUHAcVqIiulWovp1WaeUHiuJ"
+FILE_NAME = "reduced_votes_se.txt"
+LOCAL_FILE = "/app/reduced_votes_se.txt"
+OUTPUT_FILE = "display_votes.json"
+
+creds = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE)
+service = build("drive", "v3", credentials=creds)
+
+# Download bestand van Drive
+response = service.files().list(
+    q=f"name='{FILE_NAME}' and '{INPUT_FOLDER_ID}' in parents",
+    spaces="drive",
+    fields="files(id, name)"
+).execute()
+file_id = response["files"][0]["id"]
+request = service.files().get_media(fileId=file_id)
+fh = open(LOCAL_FILE, "wb")
+downloader = MediaIoBaseDownload(fh, request)
+done = False
+while not done:
+    status, done = downloader.next_chunk()
+print("📥 Bestand gedownload uit reduced_votes.")
 
 try:
     # Open and load the JSON data
@@ -21,3 +47,16 @@ except FileNotFoundError:
     print(f"File '{input_file}' not found.")
 except json.JSONDecodeError:
     print(f"Error decoding JSON from '{input_file}'.")
+
+# Upload naar Drive
+file_metadata = {
+    "name": OUTPUT_FILE,
+    "parents": [OUTPUT_FOLDER_ID]
+}
+media = MediaFileUpload(OUTPUT_FILE, mimetype="application/json")
+uploaded_file = service.files().create(
+    body=file_metadata,
+    media_body=media,
+    fields="id"
+).execute()
+print(f"✅ Bestand geüpload naar 'display_votes' map. Bestand-ID: {uploaded_file['id']}")
